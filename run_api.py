4d1d8c0f4dd1d85d40d829bb4e6d88704b5dd7b8
Mahadev Maitri <mahadevmaitri@gmail.com>
2024-03-03 04:15:50 -0500

    vercel deployment

--- run_api.py
+++ run_api.py
@@ -6,130 +6,22 @@ from io import BytesIO
 import base64
 from bson import Binary
 
-import requests
-import json
-from bs4 import BeautifulSoup
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from nltk.tokenize import word_tokenize
-import fitz
-
 app = Flask(__name__)
 CORS(app)
 
-# import nltk
-# import ssl
-
-# try:
-#     _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     pass
-# else:
-#     ssl._create_default_https_context = _create_unverified_https_context
-
-# nltk.download('punkt')
 
 mongo_uri = 'mongodb+srv://sm4825:VDSIdNRsYU9v9EVr@cluster0.gvo92qb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
 
 db_name = 'test-db'
 collection_name= 'test-connection'
 pdf_collection_name = 'fs.chunks'
 
+
 client = MongoClient(mongo_uri)
 db = client[db_name]
 collection = db[collection_name]
 pdf_collection = db[pdf_collection_name]
-
-WORKDAY_JOBS = {
-  "WALMART": [
-    "https://walmart.wd5.myworkdayjobs.com/wday/cxs/walmart/WalmartExternal/jobs",
-    "https://walmart.wd5.myworkdayjobs.com/wday/cxs/walmart/WalmartExternal/job",
-  ],
-  "NVIDIA": [
-    "https://nvidia.wd5.myworkdayjobs.com/wday/cxs/nvidia/NVIDIAExternalCareerSite/jobs",
-    "https://nvidia.wd5.myworkdayjobs.com/wday/cxs/nvidia/NVIDIAExternalCareerSite/job",
-  ],
-  "Salesforce": [
-     "https://salesforce.wd12.myworkdayjobs.com/wday/cxs/salesforce/External_Career_Site/jobs",
-     "https://salesforce.wd12.myworkdayjobs.com/wday/cxs/salesforce/External_Career_Site/job",
-  ],
-  "Henryschein": [
-     "https://henryschein.wd1.myworkdayjobs.com/wday/cxs/henryschein/External_Careers/jobs",
-     "https://henryschein.wd1.myworkdayjobs.com/wday/cxs/henryschein/External_Careers/job",
-  ],
-  "Airbus": [
-     "https://ag.wd3.myworkdayjobs.com/wday/cxs/ag/Airbus/jobs",
-     "https://ag.wd3.myworkdayjobs.com/wday/cxs/ag/Airbus/job",
-  ],
-  "Exact Science": [
-     "https://exactsciences.wd1.myworkdayjobs.com/wday/cxs/exactsciences/Exact_Sciences/jobs",
-     "https://exactsciences.wd1.myworkdayjobs.com/wday/cxs/exactsciences/Exact_Sciences/job",
-  ],
-  "Zoetis": [
-     "https://zoetis.wd5.myworkdayjobs.com/wday/cxs/zoetis/zoetis/jobs",
-     "https://zoetis.wd5.myworkdayjobs.com/wday/cxs/zoetis/zoetis/job",
-  ],
-  "Aveva": [
-     "https://aveva.wd3.myworkdayjobs.com/wday/cxs/aveva/AVEVA_careers/jobs",
-     "https://aveva.wd3.myworkdayjobs.com/wday/cxs/aveva/AVEVA_careers/job",
-  ],
-  "Orion": [
-     "https://orionadvisor.wd1.myworkdayjobs.com/wday/cxs/orionadvisor/Orion_Careers/jobs",
-     "https://orionadvisor.wd1.myworkdayjobs.com/wday/cxs/orionadvisor/Orion_Careers/job",
-  ],
-  "Adobe": [
-     "https://adobe.wd5.myworkdayjobs.com/wday/cxs/adobe/external_experienced/jobs",
-     "https://adobe.wd5.myworkdayjobs.com/wday/cxs/adobe/external_experienced/job",
-  ],
-  "Enbridge": [
-     "https://enbridge.wd3.myworkdayjobs.com/wday/cxs/enbridge/enbridge_careers/jobs",
-     "https://enbridge.wd3.myworkdayjobs.com/wday/cxs/enbridge/enbridge_careers/job",
-  ],
-  "Zillow": [
-     "https://zillow.wd5.myworkdayjobs.com/wday/cxs/zillow/Zillow_Group_External/jobs",
-     "https://zillow.wd5.myworkdayjobs.com/wday/cxs/zillow/Zillow_Group_External/job",
-  ],
-}
-
-BODY = {
-  "appliedFacets": {}, 
-  "limit": 20, 
-  "offset": 0, 
-  "searchText": "",
-}
-
-HEADERS = {
-  "Content-Type": "application/json",
-  "Accept": "*/*",
-  "Accept-Language": "en-US",
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-}
-
-def calculate_match_score(pdf_text, job_description):
-    set1 = word_tokenize(pdf_text.lower())
-    set2 = word_tokenize(job_description.lower())
-
-    set1_str = ' '.join(set1)
-    set2_str = ' '.join(set2)
-
-    vectorizer = TfidfVectorizer()
-    vectors = vectorizer.fit_transform([set1_str, set2_str])
-
-    cosine_similarity_score = cosine_similarity(vectors[0], vectors[1])[0, 0]
-    return cosine_similarity_score
-
-def scale_scores(jobs, sc, new_min, new_max):
-    old_min = min(sc)
-    old_max = max(sc)
-    for j in jobs:
-        new_score = round(((j["match_score"] - old_min) * (new_max - new_min) / (old_max - old_min)) + new_min,3)
-        j["match_score"] = new_score
-    return jobs
-
-def remove_html_tags(input_string):
-    soup = BeautifulSoup(input_string, 'html.parser')
-    text_without_tags = soup.get_text()
-    return text_without_tags
 
 @app.route('/api/data', methods=['GET'])
 def get_all_data():
@@ -236,45 +128,7 @@ def login():
     else:
         return jsonify({"error": "Invalid email or password"}), 401
 
-# @app.route('/getjobs', methods=['POST'])
-# def get_jobs():
-#   links = []
-#   BODY["searchText"] = "Software Engineer"
-#   for company in WORKDAY_JOBS:
-#     response = requests.post(WORKDAY_JOBS[company][0], data=json.dumps(BODY), headers=HEADERS)
-#     data = response.json()
-#     for job in data["jobPostings"]:
-#       job_data = {}
-#       job_data["title"] = job["title"]
-#       job_data["location"] = job["locationsText"]
-#       job_data["url"] = WORKDAY_JOBS[company][1] + "/" + job["externalPath"].split("/")[-1]
-#       job_data["company"] = company
-#       job_data["postedOn"] = job["postedOn"]
-#       resp = requests.get(job_data["url"])
-#       resp_data = resp.json()
-#       job_data["description"] = remove_html_tags(resp_data["jobPostingInfo"]["jobDescription"])
-#       job_data["job_link"] = resp_data["jobPostingInfo"]["externalUrl"]
-#       links.append(job_data)
-  
-#   resume_text = fitz.open("resume.pdf")
-#   pdf_text = ""
-#   for page_num in range(len(resume_text)):
-#       page = resume_text.load_page(page_num)
-#       pdf_text += page.get_text()
-#   resume_text.close()
-
-#   scores = []
-#   for link in links:
-#     match_score = calculate_match_score(pdf_text, link["description"])
-#     match_percentage = match_score * 100
-#     link["match_score"] = match_percentage
-#     scores.append(match_percentage)
-
-#   updated_links = scale_scores(links, scores, 30, 85)
-
-#   updated_links = [i for i in updated_links if i["match_score"] > 50]
-#   updated_links.sort(key=lambda x: x["match_score"], reverse=True)
-#   return updated_links
+    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
